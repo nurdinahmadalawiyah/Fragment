33133c117dff608a099e50c661997a070038c7aa
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2022-02-02 09:50:00 +0700

    Modified Method Call Activity From Fragment

--- app/src/main/java/com/nurdin/myflexiblefragment/DetailCategoryFragment.kt
+++ app/src/main/java/com/nurdin/myflexiblefragment/DetailCategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.nurdin.myflexiblefragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -36,8 +37,11 @@ class DetailCategoryFragment : Fragment() {
         tvCategoryName = view.findViewById(R.id.tv_category_name)
         tvCategoryDescription = view.findViewById(R.id.tv_category_description)
         btnProfile = view.findViewById(R.id.btn_profile)
+        btnProfile.setOnClickListener{
+            val mIntent = Intent(requireActivity(), ProfileActivity::class.java)
+            startActivity(mIntent)
+        }
         btnShowDialog = view.findViewById(R.id.btn_show_dialog)
-
         btnShowDialog.setOnClickListener{
             val mOptionDialogFragment = OptionDialogFragment()
 
